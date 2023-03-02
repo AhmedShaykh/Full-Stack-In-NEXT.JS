6876b1cb2470686940118f7551164ec7a1c24ee2
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-03-02 21:20:10 +0500

    Initial Commit

---
+++ Flex & Grid In Tailwind/README.md
@@ -0,0 +1,1 @@
+# Flex & Grid In Tailwind CSS!
---
+++ Flex & Grid In Tailwind/app/page.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+const Home: FC = () => {
+    return (
+        <main className='py-4 px-2 bg-black h-screen'>
+            <div className='flex justify-around text-white'>
+                <h1 className='text-4xl font-semibold'>
+                    Flex & Grid In Tailwind CSS
+                </h1>
+            </div>
+        </main>
+    )
+};
+
+export default Home;
