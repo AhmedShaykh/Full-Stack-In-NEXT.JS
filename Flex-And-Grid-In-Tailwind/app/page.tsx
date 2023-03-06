c6d94e0c6c93e4ae3352047f84678c6857c3901e
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-03-06 16:55:26 +0500

    Add Flex & Grid Component Route

--- Flex-And-Grid-In-Tailwind/app/page.tsx
+++ Flex-And-Grid-In-Tailwind/app/page.tsx
@@ -1,12 +1,43 @@
 import { FC } from 'react';
+import Link from 'next/link';
 
 const Home: FC = () => {
     return (
         <main className='py-4 px-2 bg-black h-screen'>
-            <div className='flex justify-around text-white'>
+            <div className='flex justify-center text-white'>
                 <h1 className='text-4xl font-semibold'>
                     Flex & Grid In Tailwind CSS
                 </h1>
+            </div>
+
+            <div className="pt-8 flex justify-around">
+                <div className="text-white">
+                    <h1 className='text-4xl font-semibold'>
+                        Flexbox
+                    </h1>
+
+                    <div className="flex justify-center">
+                        <h2 className="text-white py-4 text-xl">
+                            <Link href="/flex/chapter_1">
+                                1) Display Flex
+                            </Link>
+                        </h2>
+                    </div>
+                </div>
+
+                <div className="text-white">
+                    <h1 className='text-4xl font-semibold'>
+                        Grid System
+                    </h1>
+
+                    <div className="flex justify-center">
+                        <h2 className="text-white py-4 text-xl">
+                            <Link href="/grid/chapter_1">
+                                1) Display Grid
+                            </Link>
+                        </h2>
+                    </div>
+                </div>
             </div>
         </main>
     )
