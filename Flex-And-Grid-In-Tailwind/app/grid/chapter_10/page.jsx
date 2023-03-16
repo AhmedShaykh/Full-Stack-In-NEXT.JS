5d760450629cac1a1dd1314199edc845dc84a559
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-03-17 00:07:02 +0500

    Add Grid

--- Flex-And-Grid-In-Tailwind/app/grid/chapter_10/page.jsx
+++ Flex-And-Grid-In-Tailwind/app/grid/chapter_10/page.jsx
@@ -2,7 +2,46 @@ import React from 'react';
 
 const page = () => {
     return (
-        <div></div>
+        <div>
+            <form className="max-w-2xl my-8 mx-auto p-8 border border-gray-300 grid grid-cols-2 gap-6 bg-slate-100">
+                <div>
+                    <label className="block"> Full Name </label>
+                    <input className="mt-1 py-2 px-3 border border-gray-300 rounded-md w-full resize-none" type="text" name="name" placeholder="Full Name" />
+                </div>
+                <div >
+                    <label className="block"> Email Address </label>
+                    <input className="mt-1 py-2 px-3 border border-gray-300 rounded-md w-full resize-none" type="email" name="email" placeholder="Email Address" />
+                </div>
+                <div className="col-start-2 row-start-1 row-end-3">
+                    <label className="block"> Message </label>
+                    <textarea className="mt-1 py-2 px-3 border border-gray-300 rounded-md w-full resize-none" name="message" placeholder="Your Message" rowsName="5"></textarea>
+                </div>
+                <button className="col-span-full inline-block py-3 px-9 bg-red-500 text-white font-bold rounded-full">Send a Message</button>
+            </form>
+
+            <section className=" max-w-6xl mx-auto grid sm:grid-cols-2 md:grid-cols-3 gap-x-8 my-4 p-8 bg-white">
+                <img
+                    className="w-full max-w-md m-auto sm:row-start-1 sm:row-end-3 md:row-end-2 md:col-start-2"
+                    src="https://res.cloudinary.com/thirus/image/upload/v1635003545/images/mobile-ux_yc0c3w.png"
+                />
+                <div className="md:col-start-1 md:text-right">
+                    <h3 className="font-bold text-xl mt-4 text-blue-800">List Building</h3>
+                    <p className="mt-1">It's very easy to start creating email lists for your marketing actions, give it a try</p>
+                    <h3 className="font-bold text-xl mt-4 text-blue-800">Campaign Tracker</h3>
+                    <p className="mt-1">Campaigns is a feature we've created since the beginning and it's at the core of Lomar</p>
+                    <h3 className="font-bold text-xl mt-4 text-blue-800">Analytics Tool</h3>
+                    <p className="mt-1">Lomar collects all the necessary data to help you analyse different situations</p>
+                </div>
+                <div className="sm:col-start-2 md:col-start-3">
+                    <h3 className="font-bold text-xl mt-4 text-blue-800">Admin Control</h3>
+                    <p className="mt-1">Rights of users and admins can easily be managed through the control panel</p>
+                    <h3 className="font-bold text-xl mt-4 text-blue-800">Integration Setup</h3>
+                    <p className="mt-1">We're providing a step-by-step integration session to implement automation</p>
+                    <h3 className="font-bold text-xl mt-4 text-blue-800">Help Line Support</h3>
+                    <p className="mt-1">Quality support is our top priority so please contact us for any problem you encounter</p>
+                </div>
+            </section>
+        </div>
     )
 };
 
--- Flex-And-Grid-In-Tailwind/app/grid/chapter_9/page.jsx
+++ Flex-And-Grid-In-Tailwind/app/grid/chapter_9/page.jsx
@@ -1,8 +1,73 @@
 import React from 'react';
 
+const form = `max-w-lg my-8 mx-auto p-8 border border-gray-300 bg-slate-100`;
+
 const page = () => {
     return (
-        <div></div>
+        <div>
+            <form className={`${form} grid grid-cols-[auto,1fr] items-center gap-y-6 gap-x-12`}>
+                <label> Full Name </label>
+                <input className="py-2 px-3 border border-gray-300 rounded" type="text" name="name" placeholder="Full Name" />
+                <label> Email Address </label>
+                <input className="py-2 px-3 border border-gray-300 rounded" type="email" name="email" placeholder="Email Address" />
+                <button type="submit" className="col-start-2 inline-block mt-4 py-3 px-9 bg-red-500 text-white font-bold rounded-full">Create Account</button>
+            </form>
+
+            <div className="max-w-2xl mx-auto grid sm:grid-cols-2 my-8">
+                <div className="bg-white text-gray-500 sm:col-start-1 sm:col-end-3 p-10">
+                    <h2 className='font-bold text-3xl text-teal-600'>Join our community</h2>
+                    <h3 className='font-bold text-xl text-[#c0df34] my-4'>30-day, hassle-free money back guarantee</h3>
+                    <p className='mt-1'>Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
+                </div>
+                <div className="bg-teal-500 text-white p-10">
+                    <h3 className='font-bold text-xl'>Monthly Subscription</h3>
+                    <span className="inline-block mt-2.5 align-middle font-bold text-4xl">$29</span><span className="inline-block mt-2.5 align-middle pl-3">
+                        per month
+                    </span>
+                    <p className='mt-1'>Full access for less than $1 a day</p>
+                    <a href="#" className="mt-6 block py-3 px-6 text-center bg-[#c0df34] rounded font-bold">Sign Up</a>
+                </div>
+                <div className="bg-teal-400 p-10">
+                    <h3 className='font-bold text-xl'>Why Us</h3>
+                    <ol className="mt-3 text-sm">
+                        <li className='text-white'>Tutorials by industry experts</li>
+                        <li className='text-white'>Peer & expert code review</li>
+                        <li className='text-white'>Coding exercises</li>
+                        <li className='text-white'>Access to our GitHub repos</li>
+                        <li className='text-white'>Community forum</li>
+                        <li className='text-white'>Flashcard decks</li>
+                        <li className='text-white'>New videos every week</li>
+                    </ol>
+                </div>
+            </div>
+
+            <div className="max-w-none w-auto min-h-screen grid grid-cols-[22rem,1fr] grid-rows-[auto,1fr,auto]">
+                <header className="col-span-full bg-gray-600 text-white py-6 px-10">
+                    <h2 className="font-bold text-2xl">Header</h2>
+                </header>
+                <div className="bg-gray-300 p-10">
+                    <h2 className="font-bold text-2xl">Sidebar</h2>
+                    <ol>
+                        <li className="h-1 my-10 bg-white bg-opacity-50"></li>
+                        <li className="h-1 my-10 bg-white bg-opacity-50"></li>
+                        <li className="h-1 my-10 bg-white bg-opacity-50"></li>
+                        <li className="h-1 my-10 bg-white bg-opacity-50"></li>
+                    </ol>
+                </div>
+                <div className="p-10">
+                    <h2 className="font-bold text-2xl">Main Content</h2>
+                    <p className="h-1 my-10 bg-black bg-opacity-10"></p>
+                    <p className="h-1 my-10 bg-black bg-opacity-10"></p>
+                    <p className="h-1 my-10 bg-black bg-opacity-10"></p>
+                    <p className="h-1 my-10 bg-black bg-opacity-10"></p>
+                    <p className="h-1 my-10 bg-black bg-opacity-10"></p>
+                    <p className="h-1 my-10 bg-black bg-opacity-10"></p>
+                </div>
+                <footer className="col-span-full bg-gray-600 text-white py-6 px-10">
+                    <h2 className="font-bold text-2xl">Footer</h2>
+                </footer>
+            </div>
+        </div>
     )
 };
 
