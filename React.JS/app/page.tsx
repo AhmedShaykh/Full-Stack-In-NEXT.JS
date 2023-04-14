ab5887b49a8664e5ef6b814a6ce1082d7cb7b53d
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-04-15 00:26:25 +0500

    Initial Commit

---
+++ React.JS/app/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { PRODUCTS } from "../Components/products";
+import FilterableProductTable from '../Components/FilterableProductTable';
+
+const page = () => {
+    return (
+        <div className='my-4 flex justify-center space-x-2'>
+            <head>
+                <title>
+                    React.JS 18!
+                </title>
+            </head>
+
+            <FilterableProductTable products={PRODUCTS} />
+        </div>
+    )
+};
+
+export default page;
