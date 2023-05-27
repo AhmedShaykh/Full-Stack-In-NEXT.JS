20b8c3e65833bbf436e891c509e8db57d3055db4
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-05-27 20:44:01 +0500

    Add Sanity

--- Headless-CMS/next.config.js
+++ Headless-CMS/next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        appDir: true,
+    },
+    env: {
+        NEXT_PUBLIC_SANITY_PROJECT_ID: "zokqw1p0",
+        NEXT_PUBLIC_SANITY_DATASET: "production"
+    },
+};
 
 module.exports = nextConfig;
