cf4f5b4cb437541b3a6ab15b9366137fa66c0c64
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-05-30 20:57:33 +0500

    Create Sanity Client

--- Sanity CMS/next.config.js
+++ Sanity CMS/next.config.js
@@ -6,8 +6,10 @@ const nextConfig = {
     },
     env: {
         NEXT_PUBLIC_SANITY_PROJECT_ID: "zokqw1p0",
-        NEXT_PUBLIC_SANITY_DATASET: "production"
-    },
+        NEXT_PUBLIC_SANITY_DATASET: "production",
+        NEXT_PUBLIC_API_VERSION: "2023-05-27",
+        NEXT_PUBLIC_SANITY_ACCESS_TOKEN: "sk6bSUbJpqHByngIpM9fc3uZV04MkITZlIT9ZJsGjTxEY1HII0TBzbHw9FBY8ktGCLZLlJsCFJxfJ6vK2G3flcmkqrfQagkcUdLcv7h3MI6va9ySQjJlxeB1fppL2zQhXpDPS5EV65186DanvoGHYYjRbTjaKgoQ5ZddKzgg5OaB1ovaopH4"
+    }
 };
 
 module.exports = nextConfig;
