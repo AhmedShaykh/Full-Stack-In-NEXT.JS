5ee6c58f97e570e6497db124740580cc182f071d
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-05-31 00:27:14 +0500

    Fetch Data In Client

--- Sanity CMS/app/page.tsx
+++ Sanity CMS/app/page.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { client } from '@/lib/sanityClient';
 
-const Home = () => {
+export const getProductData = async () => {
+
+    const res = await client.fetch(`
+    *[_type=="product" && _id=="469d9bab-a5db-4c4b-9369-82416e75232f"][0]
+    `);
+
+    return res;
+};
+
+async function Home() {
+
+    const data = await getProductData();
+    console.log(data);
+
     return (
         <div className="flex justify-center py-4">
             <h1 className="text-4xl font-bold">
