4b20f682ec8758f01dca1cbe1bd363c1f72f68ca
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-05 00:57:21 +0500

    Add Drizzle

--- Sanity CMS/app/api/cart/route.ts
+++ Sanity CMS/app/api/cart/route.ts
@@ -1,5 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/lib/drizzle";
+import { sql } from "@vercel/postgres";
 
 export const GET = async (request: NextRequest) => {
 
+    try {
+
+    } catch (error) {
+
+    }
 };
