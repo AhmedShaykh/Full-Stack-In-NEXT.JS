fd3d744836fcfaa2e0f1983c5f64b59e773dda61
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-04 20:38:13 +0500

    Add Category Schema

--- Sanity CMS/sanity/product.ts
+++ Sanity CMS/sanity/product.ts
@@ -1,27 +1,39 @@
-export const product = {
+import { defineType, defineField } from "sanity";
+
+export const product = defineType({
     name: "product",
     type: "document",
     title: "Product",
     fields: [
-        {
+        defineField({
             name: "title",
             title: "Product Title",
             type: "string"
-        },
-        {
+        }),
+        defineField({
             name: "description",
             title: "Product Description",
             type: "string"
-        },
-        {
+        }),
+        defineField({
             name: "image",
             title: "Product Image",
-            type: "array",
-            of: [{
-                name: "img",
-                title: "Image",
-                type: "image"
-            }]
-        }
+            type: "image"
+        }),
+        defineField({
+            name: "price",
+            title: "Product Price",
+            type: "number"
+        }),
+        defineField({
+            name: "category",
+            title: "Product Category",
+            type: "reference",
+            to: [
+                {
+                    type: "category"
+                }
+            ]
+        })
     ]
-};
+});
