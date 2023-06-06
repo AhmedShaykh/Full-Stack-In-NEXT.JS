c59833c9f99941e3b518d4e2cbc6df4d11573f21
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-06 19:42:23 +0500

    Add POST API

--- Sanity CMS/app/api/getcart/route.ts
+++ Sanity CMS/app/api/getcart/route.ts
@@ -2,12 +2,21 @@ import { NextRequest, NextResponse } from "next/server";
 import { db, cartTable } from "@/lib/drizzle";
 import { v4 as uuid } from "uuid";
 import { cookies } from "next/dist/client/components/headers";
+import { eq } from "drizzle-orm";
+
+const sortId = cookies().get("user_id")?.value;
+
+fetch(`http://localhost:3000/api/cart?user_id=${sortId}`);
 
 export const GET = async (request: NextRequest) => {
 
+    const req = request.nextUrl;
+
+    const reqData = req.searchParams.get("user_id") as string;
+
     try {
 
-        const res = await db.select().from(cartTable);
+        const res = await db.select().from(cartTable).where(eq(cartTable.user_id, reqData))
 
         return NextResponse.json({ res });
 
